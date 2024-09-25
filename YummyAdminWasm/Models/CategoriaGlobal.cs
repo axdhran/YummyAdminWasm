0aa600d1bdf5113a816ff8230031fe9be822fc87
Adrian Hernandez <131229205+dev66Ops@users.noreply.github.com>
2024-09-25 16:56:30 -0600

    Proyecto Admin Yummy

---
+++ YummyAdminWasm/Models/CategoriaGlobal.cs
@@ -0,0 +1,10 @@
+// Espacio para los using
+
+namespace YummyAdmindWasm.Models
+{
+    public class CategoriaGlobal
+    {
+        public int id { get; set; }
+        public string? nombre { get; set; }
+    }
+}
---
+++ YummyAdminWasm/Program.cs
@@ -0,0 +1,21 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using YummyAdminWasm;
+using YummyAdminWasm.Auth;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080/api/") });
+builder.Services.AddScoped<UserAuth>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
