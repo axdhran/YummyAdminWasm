ce57a6ae4b992c490e7fc8ca79c093efb4d2bded
Osmany Lopez <osmany203lopez@gmail.com>
2024-11-12 22:48:17 -0600

    Implementacion de mantenimiento producto (con errores)

---
+++ YummyAdminWasm/Models/Producto.cs
@@ -0,0 +1,28 @@
+﻿namespace YummyAdminWasm.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+
+        public EstadoProducto estado { get; set; }
+
+        public string imagen { get; set; }
+        public int categoriaProductoId { get; set; }
+        public int comercioId { get; set; }
+
+        public Comercio comercio { get; set; }
+
+        public CategoriaProducto categoriaProducto { get; set; }
+
+
+
+        public enum EstadoProducto
+        {
+            ACTIVO,
+            INACTIVO
+        }
+    }
+}
